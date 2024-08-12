f595fd51f55965b87368d875935c3a86d8c184b3
heyethereum <heyethereum@gmail.com>
2024-08-12 21:14:18 +0800

    changed to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Banner from "./Banner";
 import About from "./About";
